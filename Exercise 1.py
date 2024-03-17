8374803a68d4d56c256058b528e48cd638876151
Kelvin.A <134262593+KelvinAbidha@users.noreply.github.com>
2024-03-17 07:22:02 +0300

    Add files via upload
    1.Simple Calculator: Write a program that takes two numbers and adds them together.

---
+++ Exercise 1.py
@@ -0,0 +1,5 @@
+#.Write a program in Python that takes two numbers as input from the user and performs basic arithmetic operations (addition, subtraction, multiplication, and division). Print the results for each operation
+
+def calculations(a,b):
+    print(a+b, a*b, a/b, a-b)
+calculations(1,4)
